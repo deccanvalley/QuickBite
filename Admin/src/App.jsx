98622c4d12a6d9b19f7e04f5bc76ec989a197fb6
Bhavya Pandey <pandeybhavya926@gmail.com>
2026-04-23 00:22:50 +0530

    update API URL for deployment

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const URl = "https://quickbite-or9w.onrender.com/"
+  const URl = "https://quickbite-or9w.onrender.com"
 
 
   return (
--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const [cartItem, setCartItems] = useState({});
-  const URl = "https://quickbite-or9w.onrender.com/"
+  const URl = "https://quickbite-or9w.onrender.com"
   const [token , setToken] = useState("")
   const [food_list,setFoodList] = useState([])
 
